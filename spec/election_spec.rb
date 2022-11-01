b9ac7dfdc5bee7e41b74cde8dc469013c7dfe5c8
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-01 10:09:07 -0600

    Test: wrote test for #add_race

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe Election do
         expect(election.races).to eq([])
     end
 
+    describe '#add_race' do
+        it 'an election can add races' do
+            election = Election.new('2022')
+            race1 = Race.new("Virginia District 4 Representative")
+            race2 = Race.new("Texas Governor")
+
+            election.add_race(race1)
+            election.add_race(race2)
+
+            expect(election.races).to eq([race1, race2])
+        end
+    end
 end
