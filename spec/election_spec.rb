8f82b1f0b1767ca52baeae698b7f898fc53e0c16
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-01 09:17:54 -0600

    Test: wrote test for diana.party

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -3,11 +3,16 @@ require 'pry'
 
 RSpec.describe Candidate do
 
-    it 'candidate exists' do
+    it 'candidate exists and has a name' do
         diana = Candidate.new ({name: "Diana D", party: :democrat})
         
         expect(diana.name).to eq("Diana D")
     end
 
+    it 'candidate has a party' do
+        diana = Candidate.new ({name: "Diana D", party: :democrat})
+
+        expect(diana.party).to eq(:democrat)
+    end
 
 end
