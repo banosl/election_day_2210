e7be00d02ce9455ce719be05de6fdd180034a8af
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-01 09:30:20 -0600

    Test: wrote test for Race class existing

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -1,2 +1,16 @@
 require './lib/race'
+require './lib/candidate'
 require 'pry'
+
+RSpec.describe Race do
+
+    it 'race exists' do
+        race = Race.new("Texas Governor")
+
+        expect(race).to be_instance_of(Race)
+    end
+    
+
+
+
+end
