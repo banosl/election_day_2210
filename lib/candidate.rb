d5c4d520255294ecb7adb0d61d489a9bd8da0149
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-01 09:20:57 -0600

    Feat: added votes attribute

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,9 +1,11 @@
 class Candidate
 attr_reader :name,
-            :party
+            :party,
+            :votes
 
 def initialize(attributes)
     @name = attributes[:name]
     @party = attributes[:party]
+    @votes = 0
 end
 end
