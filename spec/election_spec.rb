34e3a360a867e3a3662d27b19ca0394aac95f7e4
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-01 10:03:52 -0600

    Test: wrote test for election.year

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Election do
         expect(election).to be_instance_of(Election)
     end
 
+    it 'elections have a year' do
+        election = Election.new('2022')
+
+        expect(election.year).to eq('2022')
+    end
+
 end
